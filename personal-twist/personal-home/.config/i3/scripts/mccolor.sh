cdeb81f727643cbf530946dc8848479078ad2963
PeterDauwe <bobo5290461@gmail.com>
2021-10-17 10:01:39 +0200

    up

---
+++ personal-twist/personal-home/.config/i3/scripts/mccolor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+source "$HOME/.cache/wal/colors.sh"
+
+
+col0=${color0:1:8}
+col1=${color1:1:8}
+col2=${color2:1:8}
+col3=${color3:1:8}
+col4=${color4:1:8}
+col5=${color5:1:8}
+col6=${color6:1:8}
+col7=${color7:1:8}
+col8=${color8:1:8}
+col9=${color9:1:8}
+col10=${color10:1:8}
+col15=${color15:1:8}
+
+
+
+
+mc --colors normal=blue,default:selected=brightblue,gray:marked=yellow,default:markselect=yellow,gray:directory=brightblue,default:executable=brightgreen,default:link=cyan,default:device=brightmagenta,default:special=lightgray,default:errors=red,default:reverse=green,default:gauge=green,default:input=white,gray:dnormal=green,gray:dfocus=brightgreen,gray:dhotnormal=cyan,gray:dhotfocus=brightcyan,gray:menu=green,default:menuhot=cyan,default:menusel=green,gray:menuhotsel=cyan,default:helpnormal=cyan,default:editnormal=green,default:editbold=blue,default:editmarked=gray,blue:stalelink=red,default
